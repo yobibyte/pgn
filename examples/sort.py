d9bdc727e3eec6be0240265cd3f346ef93c0ad02
yobibyte <vitaliykurin@gmail.com>
2018-12-26 11:40:40 +0000

    refactor net init

--- examples/sort.py
+++ examples/sort.py
@@ -26,7 +26,7 @@ def graph_from_list(input_list):
     -------
     pgn.Graph with the input_list values as nodes
     """
-    connectivity = [el for el in itertools.product(range(len(input_list)), repeat=2) if el[0]!=el[1]]
+    connectivity = [el for el in itertools.product(range(len(input_list)), repeat=2) if el[0] != el[1]]
     return Graph(nodes_data=torch.Tensor([[v] for v in input_list]),
                  edges_data=torch.zeros(len(connectivity), 1),
                  connectivity=connectivity
@@ -38,11 +38,9 @@ def create_target_graph(input_graph):
 
     target_graph = copy_graph_topology(input_graph)
 
-
     nodes_data = [v.item() for v in input_graph.nodes_data]
     values = [(nid, ndata) for nid, ndata in enumerate(nodes_data)]
     min_value = min([v[1] for v in values])
-
     target_graph.nodes_data = torch.Tensor(
         # prob_true, prob_false
         [[1.0, 0.0] if v == min_value else [0.0, 1.0] for v in nodes_data])
@@ -57,10 +55,8 @@ def create_target_graph(input_graph):
             if sid != rid:
                 # get edge id by sid and rid
                 eid = input_graph.identify_edge_by_sender_and_receiver(sid, rid)
-
                 # we look for exact comparison here since we sort
-                if input_graph.nodes_data[sid] == input_graph.nodes_data[rid] or \
-                        (sidx < len(sorted_ids) - 1 and ridx == sidx + 1):
+                if (sidx < len(sorted_ids) - 1 and ridx == sidx + 1):
                     data[eid][0] = 1.0
                 else:
                     data[eid][1] = 1.0
@@ -68,12 +64,39 @@ def create_target_graph(input_graph):
 
     return target_graph
 
-N_EPOCHS = 5000
+
+def get_mlp(input_size, units, activation=nn.ReLU):
+    arch = []
+    inpt_size = input_size
+    for l in units:
+        arch.append(nn.Linear(inpt_size, l))
+        arch.append(activation())
+        inpt_size = l
+    nn.LayerNorm(inpt_size)
+    return nn.Sequential(*arch)
+
+
+def get_mlp_updaters(input_node_size,
+             input_edge_size,
+             input_global_size,
+             output_node_size,
+             output_edge_size,
+             output_global_size,
+             independent):
+    if independent:
+        edge_updater = get_mlp(input_edge_size, [16, output_edge_size])
+        node_updater = get_mlp(input_node_size, [16, output_node_size])
+        global_updater = get_mlp(input_global_size, [16, output_global_size])
+    else:
+        edge_updater = get_mlp(input_edge_size + 2*input_node_size + input_global_size, [16, output_edge_size])
+        node_updater = get_mlp(input_node_size + output_edge_size + input_global_size, [16, output_node_size])
+        global_updater = get_mlp(input_global_size + output_edge_size + output_node_size, [16, output_global_size])
+    return node_updater, edge_updater, global_updater
 
 if __name__ == '__main__':
 
     # build the graph
-    unsorted = [3.0, 1.0, 2.0, 5.0, 4.0, 24, 25, 42, 56, 1, 23]
+    unsorted = np.random.uniform(size=5)
     input_g = graph_from_list(unsorted)
     input_g.global_data = torch.Tensor([0])
 
@@ -81,115 +104,24 @@ if __name__ == '__main__':
     _, target_nodes = target_graph.nodes_data.max(dim=1)
     _, target_edges = target_graph.edges_data.max(dim=1)
 
-    input_node_size = 1
-    input_edge_size = 1
-    input_global_size = 1
-
-    output_node_size = 2
-    output_edge_size = 2
-    output_global_size = 1
-
-    # build the GN
-    # as in DM demo, 2 hidden layers of size 16 for enc/dec
-
-    enc_edge_updater = nn.Sequential(
-        nn.Linear(input_edge_size, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-    )
-
-    enc_node_updater = nn.Sequential(
-                       nn.Linear(input_node_size, 16),
-                       nn.ReLU(),
-                       nn.Linear(16, 16),
-                       nn.ReLU(),
-                       nn.LayerNorm(16),
-                   )
-
-    enc_global_updater = nn.Sequential(
-        nn.Linear(1, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-    )
-
+    enc_node_updater, enc_edge_updater, enc_global_updater = get_mlp_updaters(1, 1, 1, 16, 16, 16, independent=True)
     encoder = GraphNetwork(NodeBlock(enc_node_updater, independent=True),
                            EdgeBlock(enc_edge_updater, independent=True),
                            GlobalBlock(enc_global_updater, independent=True))
 
-    enc_global_output_size = 16
-    # build the core ###########
-    core_edge_input_size = 2*(16 + 2*16 + enc_global_output_size)
-    core_node_input_size = 16 + 32 + 2*enc_global_output_size
-
-    core_edge_updater = nn.Sequential(
-        nn.Linear(core_edge_input_size, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-    )
-
-    core_node_updater = nn.Sequential(
-        nn.Linear(core_node_input_size, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-    )
-
-    core_global_updater = nn.Sequential(
-        nn.Linear(2*enc_global_output_size + 16 + 16, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-    )
-
-    NUM_PROCESSING_STEPS = 1
+    core_node_updater, core_edge_updater, core_global_updater = get_mlp_updaters(32, 32, 32, 16, 16, 16, independent=False)
     core = GraphNetwork(NodeBlock(core_node_updater),
                         EdgeBlock(core_edge_updater),
                         GlobalBlock(core_global_updater))
 
-    # build the decoder here
-    dec_edge_updater = nn.Sequential(
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-        nn.Linear(16, 2),
-    )
-
-    dec_node_updater = nn.Sequential(
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-        nn.Linear(16, 2),
-    )
-
-
-    dec_global_updater = nn.Sequential(
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.Linear(16, 16),
-        nn.ReLU(),
-        nn.LayerNorm(16),
-        nn.Linear(16, 2),
-    )
-
-    decoder = GraphNetwork(NodeBlock(dec_node_updater, independent=True),
-                           EdgeBlock(dec_edge_updater, independent=True),
-                           GlobalBlock(dec_global_updater, independent=True),
+    dec_node_updater, dec_edge_updater, dec_global_updater = get_mlp_updaters(16, 16, 16, 16, 16, 16, independent=True)
+    decoder = GraphNetwork(NodeBlock(nn.Sequential(dec_node_updater, nn.Linear(16, 2)), independent=True),
+                           EdgeBlock(nn.Sequential(dec_edge_updater, nn.Linear(16, 2)), independent=True),
+                           GlobalBlock(nn.Sequential(dec_global_updater, nn.Linear(16, 2)), independent=True),
                            )
-
-    models = [encoder] + [core]*NUM_PROCESSING_STEPS + [decoder]
-
+    N_EPOCHS = 5000
+    NUM_PROCESSING_STEPS = 5
+    models = [encoder] + [core] * NUM_PROCESSING_STEPS + [decoder]
 
     criterion = nn.CrossEntropyLoss()
     parameters = list(encoder.parameters())+list(core.parameters())+list(decoder.parameters())
@@ -223,6 +155,6 @@ if __name__ == '__main__':
     mx = np.zeros((len(unsorted), len(unsorted)))
     for eid in range(g.num_edges):
         mx[g.senders[eid]][g.receivers[eid]] = g.edges_data[eid, 0]
-
-    plt.imshow(mx[np.argsort(unsorted)][:, np.argsort(unsorted)], cmap="viridis")
+    sort_indices = np.argsort(unsorted)
+    plt.imshow(mx[sort_indices][:, sort_indices], cmap="viridis")
     plt.show()
