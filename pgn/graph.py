7a1b8adb06a8f719727662c743c378b5d3c7a522
yobibyte <vitaliykurin@gmail.com>
2019-04-08 17:58:16 +0100

    initial implementation of generalised gns

--- pgn/graph.py
+++ pgn/graph.py
@@ -1,188 +1,259 @@
 import torch
 
-class Graph(object):
+class Entity(object):
+    def __init__(self, id, type):
+        self._id = id
+        self._type = type
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def id(self):
+        return self._id
 
 
-    def __init__(self, nodes_data=None, edges_data=None, global_data=None, connectivity=None, nodes_types=None, edges_types=None):
-        """
+class DirectedEdge(Entity):
+    def __init__(self, id, sender_id, receiver_id, connectees_type='vertex', type='edge'):
+        super().__init__(id, type)
+        self._sender_id = sender_id
+        self._receiver_id = receiver_id
+        self._connectees_type = connectees_type
 
-        Parameters
-        ----------
-        nodes_data torch.Tensor of dimensionality (n_nodes, nodes_feature_dim)
-        edges_data torch.Tensor of dimensionality (n_edges, edges_feature_dim, can be None if no edges in the graph
-        connectivity list of two-el tuples where 0-th el is the edge sender and 1-th is the edge receiver
-        index of an element correspond to its index in the edges_data tensor
-        nodes_types: list of types of the nodes
-        edges_types: list of types of the edges
-        """
+    @property
+    def sender_id(self):
+        return self._sender_id
 
-        self._nodes_data = nodes_data
-        self._edges_data = edges_data
-        self.global_data = global_data
+    @property
+    def receiver_id(self):
+        return self._receiver_id
+    
+    @property
+    def connectees_type(self):
+        return self._connectees_type
 
-        if nodes_types is not None and len(nodes_types) != self.num_nodes:
-            raise ValueError(
-                "The length of nodes_types list should be the same as the first dimension of nodes_data")
 
-        if self._edges_data is None and connectivity is not None:
-            raise ValueError("You specify connectivity for the edges, but provide no data for them")
+class Vertex(Entity):
+    def __init__(self, id, type='vertex'):
+        super().__init__(id, type)
 
-        if self._edges_data is not None:
 
-            if connectivity is None:
-                raise ValueError("All edges should have senders/receivers.")
+class Context(Entity):
+    def __init__(self, id, type='context'):
+        super().__init__(id, type)
 
-            if len(connectivity) != self.num_edges:
-                raise ValueError(
-                    "The length of edges_connectivity list should be the same as the first dimension of edges_data")
 
-            if (edges_types is not None) and (len(edges_types) != self.num_edges):
-                raise ValueError(
-                    "The length of edges_types list should be the same as the first dimension of edges_data")
+class DirectedGraph(object):
+    def __init__(self, entities):
+        # entities is a dict, where the key stands for the entity type,
+        # and the value is the dict with the 'data' and 'info' which has a list of all the entities where their index
+        # in the array corresponds to the index in the fist dimension of the data tensor
+        # [{'data': tf.Tensor, 'info': []}, ]
 
-            for s, r in connectivity:
-                if s not in range(self.num_nodes):
+        self._vertices = {el['info'][0].type: el for el in entities if isinstance(el['info'][0], Vertex)}
+        self._edges = {el['info'][0].type: el for el in entities if isinstance(el['info'][0], DirectedEdge)}
+        for t, v in self._vertices.items():
+            if self.num_vertices(t) != len(v['info']):
+                raise ValueError("The first dimension of vertices data of type %s is different "
+                                 "from the number of edges in the 'info' list" % t)
+
+        for t, v in self._edges.items():
+            if self.num_edges(t) != len(v['info']):
+                raise ValueError("The first dimension of edges data of type %s is different "
+                                 "from the number of edges in the 'info' list" % t)
+
+            # get corresponding vertex type
+            valid_vcs = range(self.num_vertices(v['info'][0].connectees_type))
+            for e in v['info']:
+                if e.sender_id not in valid_vcs:
                     raise ValueError(
-                        "Sender %d is invalid. It's either its id is negative or bigger then number of your nodes. " % s)
-                if r not in range(self.num_nodes):
+                        "Sender %d for edge %d of type %s is invalid. It's either its id is negative or bigger "
+                        "then number of your nodes. " % (e.sender_id, e.id, e.type))
+                if e.receiver_id not in valid_vcs:
                     raise ValueError(
-                        "Receiver %d is invalid. It's either its id is negative or bigger then number of your nodes. " % r)
+                        "Receiver %d for edge %d of type %s is invalid. It's either its id is negative or bigger "
+                        "then number of your nodes. " % (e.receiver_id, e.id, e.type))
 
-            self._edges_info = [{'connectivity': (conn[0], conn[1]),
-                                 'type': None if edges_types is None else edges_types[eid]}
-                                for eid, conn in enumerate(connectivity)]
+    def num_vertices(self, type):
+        return self._vertices[type]['data'].shape[0]
 
-            if edges_types is not None:
-                for idx, t in enumerate(edges_types):
-                    self._edges_info[idx]['type'] = t
-
-        if nodes_data is not None:
-            self._nodes_info = [
-                {'incoming': [] if edges_data is None else [eid for eid, einfo in enumerate(self._edges_info) if einfo['connectivity'][1] == nid],
-                 'outgoing': [] if edges_data is None else [eid for eid, einfo in enumerate(self._edges_info) if einfo['connectivity'][0] == nid],
-                 'type': None if nodes_types is None else nodes_types[nid]
-                 }
-                for nid in range(self.num_nodes)]
+    def num_edges(self, type):
+        return self._edges[type]['data'].shape[0]
 
     @property
-    def nodes_data(self):
-        return self._nodes_data
-
-    @nodes_data.setter
-    def nodes_data(self, nodes_data=None):
-        #TODO check cosistency after modifications everywhere
-        self._nodes_data = nodes_data
+    def num_edge_types(self):
+        return len(self.edge_types)
 
     @property
-    def edges_data(self):
-        return self._edges_data
+    def num_vertex_types(self):
+        return len(self.vertex_types)
 
-    @edges_data.setter
-    def edges_data(self, edges_data=None):
-        # TODO check cosistency after modifications everywhere
-        self._edges_data = edges_data
+    def identify_edge_by_sender_and_receiver(self, sender_id, receiver_id, vertex_type=None):
+        if vertex_type is None and len(self.vertex_types) > 1:
+            raise ValueError("I have more than one vertex type, you need to provide a type to identify an edge")
 
-    @property
-    def global_data(self):
-        return self._global_data
+        if vertex_type is None:
+            vertices = next(iter(self._vertices.values()))
+        else:
+            vertices = self._vertices[vertex_type]
 
-    @global_data.setter
-    def global_data(self, global_data=None):
-        self._global_data = global_data
-
-    @property
-    def num_nodes(self):
-        return self._nodes_data.shape[0]
-
-    @property
-    def num_edges(self):
-        return self._edges_data.shape[0]
-
-    @property
-    def edges_types(self):
-        return [e['type'] for e in self._edges_info]
-
-    @property
-    def nodes_types(self):
-        return [n['type'] for n in self._nodes_info]
-
-    @property
-    def connectivity(self):
-        return [(e['connectivity'][0], e['connectivity'][1]) for e in self._edges_info]
-
-    @property
-    def senders(self):
-        return [e['connectivity'][0] for e in self._edges_info]
-
-    @property
-    def receivers(self):
-        return [e['connectivity'][1] for e in self._edges_info]
-
-    @property
-    def incoming(self):
-        return [n['incoming'] for n in self._nodes_info]
-
-    @property
-    def outgoing(self):
-        return [n['outgoing'] for n in self._nodes_info]
-
-    def identify_edge_by_sender_and_receiver(self, sender_id, receiver_id):
         if sender_id > self.num_edges - 1 or receiver_id > self.num_edges -1:
             return -1
 
-        conn = self.connectivity
         for eid in range(self.num_edges):
-            if conn[eid][0] == sender_id and conn[eid][1] == receiver_id:
+            if vertices[eid].sender_id == sender_id and vertices[eid].receiver_id == receiver_id:
                 return eid
 
         return -1
 
-    def _graph_summary(self):
-        for nid, ninfo in enumerate(self._nodes_info):
-            print("Node with id: %d, data: %s, incoming edges: %s, outcoming edges: %s, type: %s." %
-                  (nid, str(self._nodes_data[nid]), ninfo['incoming'], ninfo['outgoing'], ninfo['type']))
+    def edge_data(self, type=None):
+        if type is None and len(self.edge_types) > 1:
+            raise ValueError("I have more than one edge type, you need to provide a type to get the data")
+        if type is None:
+            return next(iter(self._edges.values()))['data']
+        else:
+            return self._edges[type]['data']
 
-        for eid, einfo in enumerate(self._edges_info):
-            print("Edge with id: %d, data: %s, sender id: %d, receiver id: %d, type: %s." %
-                  (eid, self._edges_data[eid], einfo['connectivity'][0], einfo['connectivity'][1], einfo['type']))
+    def vertex_data(self, type=None):
+        if type is None and len(self.vertex_types) > 1:
+            raise ValueError("I have more than one vertex type, you need to provide a type to get the data")
+        if type is None:
+            return next(iter(self._vertices.values()))['data']
+        else:
+            return self._vertices[type]['data']
 
-def copy_graph_topology(G):
-    nodes_data = torch.zeros_like(G.nodes_data)
-    edges_data = torch.zeros_like(G.edges_data)
-    global_data = torch.zeros_like(G.global_data)
-    return Graph(nodes_data, edges_data, global_data, G.connectivity, G.nodes_types, G.edges_types)
+    @property
+    def edge_types(self):
+        return list(self._edges.keys())
 
-def concat_graphs(graph_list):
-    """
-    Concatenate features of the graphs with the same topology
+    @property
+    def vertex_types(self):
+        return list(self._edges.keys())
 
-    Parameters
-    ----------
-    graph_list: list with pgn.graph.Graph entries
+    def senders(self, id=None, edge_type=None):
+        edge_type = self.default_edge_type if edge_type is None else edge_type
 
-    Returns
-    -------
-    A concatenated graph
-    """
+        if id is None:
+            return [el.sender_id for el in self._edges[edge_type]['info']]
+        else:
+            return self._edges[edge_type]['info'][id].sender_id
 
-    if len(graph_list) == 0:
-        raise ValueError("Nothing to concatenate. Give me some graphs, man!")
+    def receivers(self, id=None, edge_type=None):
+        edge_type = self.default_edge_type if edge_type is None else edge_type
 
-    if len(graph_list) == 1:
-        return graph_list[0]
+        if id is None:
+            return [el.receiver_id for el in self._edges[edge_type]['info']]
+        else:
+            return self._edges[edge_type]['info'][id].receiver_id
 
-    # TODO check that topology is the same for everyone
-    res = copy_graph_topology(graph_list[0])
-    res._nodes_data = torch.cat([g._nodes_data for g in graph_list], dim=1)
-    res._edges_data = torch.cat([g._edges_data for g in graph_list], dim=1)
-    res._global_data = torch.cat([g._global_data for g in graph_list])
-    return res
+    def incoming_edges(self, id=None, vertex_type=None):
+        vertex_type = self.default_vertex_type if vertex_type is None else vertex_type
+        incoming = []
+
+        if id is None:
+            for v_id in range(self.num_vertices(vertex_type)):
+                curr_v = []
+                for k, v in self._edges.items():
+                    curr_v.append({k: [el.id for el in v['info'] if el.receiver_id == v_id and el.connectees_type == vertex_type]})
+                if self.num_edge_types == 1:
+                    curr_v = curr_v[0][self.default_edge_type]
+                incoming.append(curr_v)
+        else:
+            for k, v in self._edges.items():
+                incoming.append({k: [el.id for el in v['info'] if el.receiver_id == id and el.connectees_type == vertex_type]})
+            if self.num_edge_types == 1:
+                return incoming[0][self.default_edge_type]
+        return incoming
+
+    def outgoing_edges(self, id=None, vertex_type=None):
+        vertex_type = self.default_vertex_type if vertex_type is None else vertex_type
+        outgoing = []
+
+        if id is None:
+            for v_id in range(self.num_vertices(vertex_type)):
+                curr_v = []
+                for k, v in self._edges.items():
+                    curr_v.append({k: [el.id for el in v['info'] if el.sender_id == v_id and el.connectees_type == vertex_type]})
+                if self.num_edge_types == 1:
+                    curr_v = curr_v[0][self.default_edge_type]
+                outgoing.append(curr_v)
+        else:
+            for k, v in self._edges.items():
+                outgoing.append({k: [el.id for el in v['info'] if el.sender_id == id and el.connectees_type == vertex_type]})
+            if self.num_edge_types == 1:
+                return outgoing[0][self.default_edge_type]
+
+        return outgoing
 
 
-def copy_graph(G):
-    return Graph(G._nodes_data.clone(),
-                 G._edges_data.clone(),
-                 G._global_data.clone(),
-                 G.connectivity,
-                 G.nodes_types,
-                 G.edges_types)
+    @property
+    def default_vertex_type(self):
+        if len(self._vertices.keys()) > 1:
+            raise ValueError("I have more than one vertex type, you need to provide a type to get the data")
+        return next(iter(self._vertices.keys()))
+
+    @property
+    def default_edge_type(self):
+        if len(self._edges.keys()) > 1:
+            raise ValueError("I have more than one edge type, you need to provide a type to get the data")
+        return next(iter(self._edges.keys()))
+
+    # def _graph_summary(self):
+    #     for vt in self.vertex_types:
+    #         print('Vertices of type %s' %vt)
+    #         for vid, vinfo in enumerate(self._vertices[vt]):
+    #             print("Node with id: %d, data: %s, incoming edges: %s, outcoming edges: %s, type: %s." %
+    #                   (vid, str(self._vertices[vt]['data'][vid]), vinfo['incoming'], vinfo['outgoing'], vinfo['type']))
+
+        # for eid, einfo in enumerate(self._edges_info):
+        #     print("Edge with id: %d, data: %s, sender id: %d, receiver id: %d, type: %s." %
+        #           (eid, self._edges_data[eid], einfo['connectivity'][0], einfo['connectivity'][1], einfo['type']))
+
+
+class DirectedGraphWithContext(DirectedGraph):
+    def __init__(self, entities):
+        super().__init__(entities)
+        self._context = [el for el in entities if isinstance(el, Context)][0]
+
+
+# def copy_graph_topology(G):
+#     nodes_data = torch.zeros_like(G.nodes_data)
+#     edges_data = torch.zeros_like(G.edges_data)
+#     global_data = torch.zeros_like(G.global_data)
+#     return Graph(nodes_data, edges_data, global_data, G.connectivity, G.nodes_types, G.edges_types)
+#
+# def concat_graphs(graph_list):
+#     """
+#     Concatenate features of the graphs with the same topology
+#
+#     Parameters
+#     ----------
+#     graph_list: list with pgn.graph.Graph entries
+#
+#     Returns
+#     -------
+#     A concatenated graph
+#     """
+#
+#     if len(graph_list) == 0:
+#         raise ValueError("Nothing to concatenate. Give me some graphs, man!")
+#
+#     if len(graph_list) == 1:
+#         return graph_list[0]
+#
+#     # TODO check that topology is the same for everyone
+#     res = copy_graph_topology(graph_list[0])
+#     res._nodes_data = torch.cat([g._nodes_data for g in graph_list], dim=1)
+#     res._edges_data = torch.cat([g._edges_data for g in graph_list], dim=1)
+#     res._global_data = torch.cat([g._global_data for g in graph_list])
+#     return res
+#
+#
+# def copy_graph(G):
+#     return Graph(G._nodes_data.clone(),
+#                  G._edges_data.clone(),
+#                  G._global_data.clone(),
+#                  G.connectivity,
+#                  G.nodes_types,
+#                  G.edges_types)
--- pgn/tests/test_graph.py
+++ pgn/tests/test_graph.py
@@ -6,35 +6,47 @@ import torch
 class TestGraph(unittest.TestCase):
 
     def setUp(self):
-        node_data = torch.Tensor([[0], [1]])
-        edges_data = torch.Tensor([[0], [1], [2]])
-        connectivity = [(0, 1), (0, 1), (1, 0)]
-        self.g = graph.Graph(node_data, edges_data, connectivity=connectivity)
+        vertex_data = torch.Tensor([[0], [1]])
+        edge_data = torch.Tensor([[0], [1], [2]])
+        conn = [(0, 1), (0, 1), (1, 0)]
+        entities = [
+                    {'data': vertex_data,
+                              'info': [graph.Vertex(el) for el in range(vertex_data.shape[0])]},
+                    {'data': edge_data,
+                               'info': [graph.DirectedEdge(el, conn[el][0], conn[el][1]) for el in range(edge_data.shape[0])]}
+                    ]
+
+        self.g = graph.DirectedGraph(entities)
 
     def test_graph_build(self):
 
-        self.assertEqual(self.g.nodes_data[0], 0)
-        self.assertEqual(self.g.nodes_data[1], 1)
+        self.assertEqual(self.g.vertex_data()[0], 0)
+        self.assertEqual(self.g.vertex_data()[1], 1)
 
-        self.assertEqual(self.g.edges_data[0], 0)
-        self.assertEqual(self.g.edges_data[1], 1)
-        self.assertEqual(self.g.edges_data[2], 2)
+        self.assertEqual(self.g.edge_data()[0], 0)
+        self.assertEqual(self.g.edge_data()[1], 1)
+        self.assertEqual(self.g.edge_data()[2], 2)
 
-        self.assertEqual(set(self.g.incoming[0]), {2})
-        self.assertEqual(set(self.g.outgoing[0]), {0, 1})
+        # test with id output
+        self.assertEqual(set(self.g.incoming_edges(0)), {2})
+        self.assertEqual(set(self.g.outgoing_edges(0)), {0, 1})
 
-        self.assertEqual(set(self.g.incoming[1]), {0, 1})
-        self.assertEqual(set(self.g.outgoing[1]), {2})
+        # test with list output
+        self.assertEqual(set(self.g.incoming_edges()[1]), {0, 1})
+        self.assertEqual(set(self.g.outgoing_edges()[1]), {2})
 
-        self.assertEqual(self.g.senders[0], 0)
-        self.assertEqual(self.g.receivers[0], 1)
-        self.assertEqual(self.g.senders[1], 0)
-        self.assertEqual(self.g.receivers[1], 1)
-        self.assertEqual(self.g.senders[2], 1)
-        self.assertEqual(self.g.receivers[2], 0)
+        # test with id output
+        self.assertEqual(self.g.senders(1), 0)
+        self.assertEqual(self.g.receivers(1), 1)
+        self.assertEqual(self.g.senders(2), 1)
+        self.assertEqual(self.g.receivers(2), 0)
 
-    def test_graph_summary(self):
-        self.g._graph_summary()
+        # test with the list output
+        self.assertEqual(self.g.senders()[0], 0)
+        self.assertEqual(self.g.receivers()[0], 1)
+
+    # def test_graph_summary(self):
+    #     self.g._graph_summary()
 
 
 if __name__ == '__main__':
