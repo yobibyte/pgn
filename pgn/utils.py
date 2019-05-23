ea3666134e990af3387bcfff1a38319dc91f7e7c
yobibyte <vitaliykurin@gmail.com>
2019-05-23 12:06:00 +0100

    move profiling to yobitools

--- pgn/utils.py
+++ pgn/utils.py
@@ -1,6 +1,3 @@
-import cProfile
-import time
-
 import networkx as nx
 import torch
 from networkx.drawing.nx_agraph import to_agraph
@@ -89,66 +86,6 @@ def plot_graph(g, fname='graph.pdf'):
     a = to_agraph(g)
     a.layout('circo')  # circo was the friendliest layout for me
     a.draw(fname)
-
-
-def profileit(name):
-    """Profiling decorator
-
-    Add @profileit('your_name') for a function to run cprof on it
-    Based on https://stackoverflow.com/a/5376616
-
-    Parameters
-    ----------
-    name: str
-        name of the file to save profiling results to
-
-    Returns
-    -------
-
-    """
-
-    #
-    def inner(func):
-        def wrapper(*args, **kwargs):
-            prof = cProfile.Profile()
-            retval = prof.runcall(func, *args, **kwargs)
-            # Note use of name from outer scope
-            prof.dump_stats(name)
-            return retval
-
-        return wrapper
-
-    return inner
-
-
-def timeit(name):
-    """Timing decorator
-
-    Add @timeit('name') to time execution of the function.
-    Built in the same way as profileit above.
-
-    Parameters
-    ----------
-    name: str
-        Name to see in the output.
-
-    Returns
-    -------
-
-    """
-
-    def inner(func):
-        def wrapper(*args, **kwargs):
-            ts = time.time()
-            result = func(*args, **kwargs)
-            te = time.time()
-            print('%s, took: %2.4f sec' % (name, te - ts))
-            return result
-
-        return wrapper
-
-    return inner
-
 
 if __name__ == '__main__':
     g = generate_graph()
