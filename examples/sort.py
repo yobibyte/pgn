fix loss computation

examples/sort.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
-from pgn.graph import DirectedGraphWithContext
 from pgn.models import EncoderCoreDecoder
 from pgn.utils import pgn2nx, plot_graph, batch_data
 
@@ -131,11 +130,14 @@ def batch_loss(outs, targets, criterion, batch_size=32):
         Shows how good your mode is.
     """
     loss = 0
+
+    vsize = targets[0].shape[0]//batch_size
+    esize = targets[1].shape[0]//batch_size
     for out in outs:
-        for i in range(out[0].shape[0]//batch_size):
-            loss+=criterion(out[0][i*batch_size:(i+1)*batch_size], targets[0][i*batch_size:(i+1)*batch_size])
-        for i in range(out[1]['default'].shape[0] // batch_size):
-            loss+=criterion(out[1]['default'][i*batch_size:(i+1)*batch_size], targets[1][i*batch_size:(i+1)*batch_size])
+        for i in range(vsize//batch_size):
+            loss+=criterion(out[0][i*vsize:(i+1)*vsize], targets[0][i*vsize:(i+1)*vsize])
+        for i in range(esize // batch_size):
+            loss+=criterion(out[1]['default'][i*esize:(i+1)*esize], targets[1][i*esize:(i+1)*esize])
     return loss
 
 
@@ -181,8 +183,6 @@ def run():
     if args.plot_graph_sample:
         ng = pgn2nx(train_input[0])
         plot_graph(ng, fname='input_graph.pdf')
-
-
 
     optimiser = torch.optim.Adam(lr=0.001, params=model.parameters())
     criterion = nn.BCEWithLogitsLoss()

pgn/blocks.py
@@ -78,7 +78,7 @@ class NodeBlock(Block):
         else:
             aggregated = []
             for at in self._in_e2n_aggregators:
-                agg = self._in_e2n_aggregators[at](edata[at], connectivity[at][1,:], dim_size=vdata.shape[0])
+                agg = self._in_e2n_aggregators[at](edata[at], connectivity[at][1, :], dim_size=vdata.shape[0])
                 aggregated.append(agg)
 
             aggregated = torch.cat(aggregated, dim=1)
@@ -130,7 +130,7 @@ class EdgeBlock(Block):
         """
 
         if self._independent:
-            return {et: self._updaters[et](ed) for et,ed in edata.items()}
+            return {et: self._updaters[et](ed) for et, ed in edata.items()}
         else:
             #TODO implemente context cdata = [g.context_data(concat=True) for g in Gs] if isinstance(Gs[0],
             #                                                                pg.DirectedGraphWithContext) else None
@@ -141,7 +141,7 @@ class EdgeBlock(Block):
                 if cdata is None:
                     out[et] = self._updaters[et](torch.cat([edata[et], vdata[senders], vdata[receivers]]))
                 else:
-                    out[et] = self._updaters[et](torch.cat([edata[et], vdata[senders], vdata[receivers], cdata[et]],dim=1))
+                    out[et] = self._updaters[et](torch.cat([edata[et], vdata[senders], vdata[receivers], cdata[et]], dim=1))
 
             return out
 
@@ -263,10 +263,6 @@ class IndependentGraphNetwork(nn.Module):
     def forward(self, vdata, edata, connectivity, cdata, metadata):
         # make one pass as in the original paper
         # 1. Compute updated edge attributes
-
-        edge_outs = None
-        v_outs = None
-        g_outs = None
 
         if self._edge_block is not None:
             edata = self._edge_block(vdata, edata, connectivity, cdata)

pgn/utils.py
@@ -35,6 +35,7 @@ def batch_data(graph_list):
         edata_d[et] = torch.cat([el[et] for el in edata])
         connectivity_d[et] = torch.cat([conn[et] for conn in connectivity],dim=1)
         # TODO replace this with roll and setting the last element to 0
+
         correction = torch.cat([torch.zeros(esizes[et][0],dtype=torch.long), *[torch.tensor([el]*esizes[et][i],dtype=torch.long) for i,el in enumerate(vcumsum[:-1])]] )
         connectivity_d[et] += correction.expand(2,-1)
 
@@ -123,7 +124,6 @@ def plot_graph(g, fname='graph.pdf'):
 
 
 def concat_entities(entities):
-
 
     has_global = len(entities[0]) == 3
 